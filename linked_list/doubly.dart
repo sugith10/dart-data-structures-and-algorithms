f437826b8ec461f9d23b2930f2daaec14a8d58bd
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-02-06 19:09:20 +0530

    fixing mistakes

--- linked_list/doubly.dart
+++ linked_list/doubly.dart
@@ -1,4 +1,4 @@
-class Node{
+class Node {
   Node? prev;
   int? data;
   Node? next;
@@ -6,57 +6,59 @@ class Node{
   Node(this.data);
 }
 
-class LinkedList{
+class LinkedList {
   Node? head;
   Node? tail;
 
-  addNode(int data){
+  addNode(int data) {
     Node newNode = Node(data);
-    if(head == null){
+    if (head == null) {
       head = newNode;
-    }else{
+    } else {
       newNode.prev = tail;
       tail?.next = newNode;
     }
     tail = newNode;
   }
 
-  display(){
-    if(head == null ){
+  display() {
+    if (head == null) {
       print("empty");
-    }else{
+    } else {
       Node? temp = head;
-      while(temp != null){
+      while (temp != null) {
         print(temp.data);
         temp = temp.next;
       }
     }
   }
-  displayBackward(){
-    if(tail == null){
+
+  displayBackward() {
+    if (tail == null) {
       print("empty");
-    }else{
+    } else {
       Node? temp = tail;
-      while(temp != null){
+      while (temp != null) {
         print(temp.data);
         temp = temp.prev;
       }
     }
   }
-  delete(int data){
-    if(head == null){
+
+  delete(int data) {
+    if (head == null) {
       print('list is empty');
-    }else{
+    } else {
       Node? temp = head;
-      while(temp != null && temp.data != data){
+      while (temp != null && temp.data != data) {
         temp = temp.next;
       }
-      if(temp == head){
+      if (temp == head) {
         head = head?.next;
         head?.prev = null;
         return;
       }
-      if(temp == tail){
+      if (temp == tail) {
         tail = tail?.prev;
         tail?.next = null;
         return;
@@ -66,107 +68,80 @@ class LinkedList{
     }
   }
 
-  insertAt(int findNext,int data){
+  void insertAt(int findNext, int data) {
     Node newNode = Node(data);
-    if(head?.data == findNext){
-      if(head == tail){
-        head?.next = newNode;
-        newNode.prev = head;
-        tail = newNode;
-      }else{
-        newNode.next = head?.next;
-        newNode.prev = head;
-        head?.next = newNode;
-      }
-    }else{
-      Node? temp;
-      temp = head;
-      while(temp != null && temp.data != findNext){
-        temp = temp.next;
-      }
-      if(temp == null){
-        print("value not found");
-      }
-      if(temp == tail){
-        tail?.next = temp;
-        temp?.prev = tail;
-        tail = temp;
-      }
-      newNode.prev = temp;
-      newNode.next = temp?.next; 
-      temp?.next = newNode;
+    Node? temp = head;
+    while (temp != null && temp.data != findNext) {
+      temp = temp.next;
     }
+    newNode.prev = temp;
+    newNode.next = temp?.next;
+    if (temp == tail) {
+      tail = newNode;
+    } else {
+      temp?.next?.prev = newNode;
+    }
+    temp?.next = newNode;
   }
 
-  sortedListDel(){
-    if(head == null){
+  sortedListDel() {
+    if (head == null) {
       print("empty");
-    }else{
+    } else {
       Node? temp = head;
-      while(temp != null ){
-        if(temp.data == temp.next?.data){
+      while (temp != null) {
+        if (temp.data == temp.next?.data) {
           Node? newTemp = temp;
-          while(temp?.next != null && temp?.data == temp?.next?.data){                       
+          while (temp?.next != null && temp?.data == temp?.next?.data) {
             temp = temp?.next;
           }
-          if(temp == tail){
+          if (temp == tail) {
             tail = newTemp;
             tail?.next = null;
             return;
           }
           newTemp.next = temp?.next;
-        }else{
+        } else {
           temp = temp.next;
         }
       }
     }
   }
 
-  sortedListDelete(){
-    if(head == null){
+  sortedListDelete() {
+    if (head == null) {
       print('empty');
-    }else{
+    } else {
       Node? temp = head;
-      while(temp != null){
-        if(temp.data == temp.next?.data){
+      while (temp != null) {
+        if (temp.data == temp.next?.data) {
           temp.next = temp.next?.next;
-        }else{
+        } else {
           temp = temp.next;
         }
       }
     }
   }
 
-  addToList(List<int>? numbers){
-    if(head == null){
+  addToList(List<int>? numbers) {
+    if (head == null) {
       print("list is empty");
-    }else{
+    } else {
       Node? temp = head;
-      while(temp!= null){
+      while (temp != null) {
         numbers?.add(temp.data!);
       }
     }
   }
-  
-
 }
 
-
-main(){
+main() {
   LinkedList list = LinkedList();
-  List<int> numbers = [];
+  //List<int> numbers = [];
 
   list.display();
   list.addNode(10);
   list.addNode(20);
-  list.addNode(30);
-  list.addNode(30);
-  list.addNode(30);
-  list.addNode(30);
-  list.addNode(30);
-  list.addNode(30);
-  list.addNode(30);
-  list.addNode(30);
   list.addNode(30);
   list.addNode(40);
   list.addNode(50);
@@ -177,10 +152,10 @@ main(){
   // list.sortedListDelete();
   // list.delete(20);
   // list.display();
-  // list.insertAt(30, 35);
-  // list.display();
-  list.addToList(numbers);
-  print(numbers);
-  // print('list backward');        
+  list.insertAt(30, 35);
+  list.display();
+  // list.addToList(numbers);
+  // print(numbers);
+  // print('list backward');
   // list.displayBackward();
 }
